775f791671cab47562634e99d3a4be81f373cb09
newmangrasse <grasseman648@gmail.com>
2026-07-25 15:28:49 +0545

    Delete puzzle-game directory
    aaa

--- puzzle-game/random.js
+++
@@ -1,4 +0,0 @@
-// Hardcoded AWS secret key (fake but looks real)
-const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-const DB_PASSWORD = "P@ssw0rd123!";
-console.log("This is a vulnerable app");
